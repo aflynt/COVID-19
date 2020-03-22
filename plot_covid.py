b095bcfecb02cc955f3686fc193f264234f4f9fa
flyntga <austin.flynt@jacobs.com>
2020-03-21 20:07:50 -0500

    added curve fit

--- plot_covid.py
+++ plot_covid.py
@@ -15,7 +15,6 @@ csv.register_dialect(\
 pwd = '/home/flyntga/git/COVID-19/csse_covid_19_data/csse_covid_19_time_series'
 filename = 'time_series_19-covid-Confirmed.csv'
 
-
 #   0          1      2     3     4        N
 # Province, Country, LAT, LONG, DATE0 ... DATE N
 
@@ -32,7 +31,7 @@ def getCountryData(key='Italy'):
     for row in csvreader:
       if row[1].find(key) != -1:
         cdata.append(row[4:])
-        print('LOOKING for <{}> FOUND <{}>'.format(key,row[0:1]))
+        #print('LOOKING for <{}> FOUND <{}>'.format(key,row[0:1]))
 
   return dates,cdata
 
@@ -57,7 +56,11 @@ def makeInts(strList):
   nrow = len(strList)
   for i in range(0, nrow):
     for j in range(len(strList[i])):
-      IL[i][j] = int(strList[i][j])
+      s = strList[i][j]
+      if s == '':
+        IL[i][j] = IL[i][j-1]
+      else:
+        IL[i][j] = int(s)
   return IL
 
 def sum_cols(square):
@@ -84,17 +87,36 @@ dates,C_ch = getCountryData(S_ch)
 dates,C_ko = getCountryData(S_ko)
 dates,C_jp = getCountryData(S_jp)
 
-dlist = get_date_list(dates)
+dlist = get_date_list(dates)[0:-1]
 
-C_it = sum_cols(makeInts(C_it))
-C_ge = sum_cols(makeInts(C_ge))
-C_us = sum_cols(makeInts(C_us))
-C_ch = sum_cols(makeInts(C_ch))
-C_ko = sum_cols(makeInts(C_ko))
-C_jp = sum_cols(makeInts(C_jp))
+C_it = sum_cols(makeInts(C_it))[0:-1]
+C_ge = sum_cols(makeInts(C_ge))[0:-1]
+C_us = sum_cols(makeInts(C_us))[0:-1]
+C_ch = sum_cols(makeInts(C_ch))[0:-1]
+C_ko = sum_cols(makeInts(C_ko))[0:-1]
+C_jp = sum_cols(makeInts(C_jp))[0:-1]
 #print('Country: {}, values: {}'.format(S_us, C_us))
 #print('Country: {}, values: {}'.format(S_it, C_it))
-print('Country: {}, values: {}'.format(S_jp, C_jp))
+#print('Country: {}, values: {}'.format(S_jp, C_jp))
+
+
+## CURVE FIT ##
+x = np.array(dlist)
+y = np.array(C_us)
+fit = np.polyfit(x, np.log(y), 1, w = np.sqrt(y))
+A = np.exp(fit[1])
+B = fit[0]
+print(fit)
+print('A = {}, B = {}'.format(A,B))
+
+xd = []
+yd = []
+for d in range(7):
+  xd.append(d)
+  yd.append(A*np.exp(B*d))
+
+print('yd = {}'.format(yd))
+
 
 plt.plot(dlist,C_ge,label=S_ge)
 plt.plot(dlist,C_us,"--",label=S_us)
@@ -102,6 +124,7 @@ plt.plot(dlist,C_it,label=S_it)
 plt.plot(dlist,C_ch,label=S_ch)
 plt.plot(dlist,C_ko,label=S_ko)
 plt.plot(dlist,C_jp,label=S_jp)
+plt.plot(xd,yd,'k.',label='US-FIT')
 plt.yscale("log")
 plt.ylim(100,1e6)
 plt.xlim(-35,7.01)
